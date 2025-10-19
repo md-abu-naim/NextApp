4c3379feb7437c656f626cb4e965f55abaf1bf45
abunaim34 <farhanadnanfarabi@gmail.com>
2025-10-19 20:58:58 +0600

    navber design

--- src/app/Navber/Navber.jsx
+++ src/app/Navber/Navber.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Navber = () => {
@@ -12,32 +13,18 @@ const Navber = () => {
                         <ul
                             tabIndex="-1"
                             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                            <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
+                            <li><Link href='/'>Home</Link></li>
+                            <li><Link href='/contacts'>Contact Us</Link></li>
+                            <li><Link href='/about'>About Us</Link></li>
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <Link href='/' className="btn btn-ghost text-2xl">NextApp</Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        <li><Link href='/'>Home</Link></li>
+                        <li><Link href='/contacts'>Contact Us</Link></li>
+                        <li><Link href='/about'>About Us</Link></li>
                     </ul>
                 </div>
                 <div className="navbar-end">
