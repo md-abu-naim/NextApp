ca5407e35197e729ec537349bfa6f921d7c42b03
abunaim34 <farhanadnanfarabi@gmail.com>
2025-10-19 21:01:16 +0600

    create contact page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navber from "./Navber/Navber";
+import Navber from "./Components/Navber";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
