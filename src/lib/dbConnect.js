29462cfadecc69d88ce013550dd2bd69cff2eb2f
abunaim34 <farhanadnanfarabi@gmail.com>
2025-10-20 21:08:10 +0600

    mongoDB connect

---
+++ src/lib/dbConnect.js
@@ -0,0 +1,18 @@
+
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const dbConnect = async () => {
+    const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.zyfftle.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
+
+    return client.db('natureGlow').collection('products');
+}
+
+export default dbConnect;
