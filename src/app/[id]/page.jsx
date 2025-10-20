bef7d6b667dc5cc978a55ca9ce92227150b71486
abunaim34 <farhanadnanfarabi@gmail.com>
2025-10-20 21:51:40 +0600

    id wise single data fetch from db

--- src/app/[id]/page.jsx
+++ src/app/[id]/page.jsx
@@ -1,23 +1,28 @@
+import dbConnect from "@/lib/dbConnect";
+import { ObjectId } from "mongodb";
+import Image from "next/image";
 
-const ProductDetails = () => {
+const ProductDetails = async({params}) => {
+    const p = await params.id
+    const id = {_id: new ObjectId(p)}
+    const data = dbConnect()
+    const product = await data.findOne(id)
+    console.log(product);
+
     return (
         <div className="min-h-screen  flex items-center justify-center p-4">
       <div className=" rounded-xl shadow-lg max-w-4xl w-full flex flex-col md:flex-row overflow-hidden">
         
         {/* Product Image */}
         <div className="md:w-1/2">
-          <img
-            src=""
-            alt=""
-            className="w-full h-full object-cover"
-          />
+          <Image className="w-full h-full object-cover" src={product.image} alt="image" width={600} height={400} />
         </div>
 
         {/* Product Details */}
         <div className="md:w-1/2 p-8 flex flex-col justify-between">
           <div>
-            <h1 className="text-3xl font-bold mb-3 ">name</h1>
-            <p className="text-sm  mb-4">Category: {}</p>
+            <h1 className="text-3xl font-bold mb-3 ">{product.name}</h1>
+            <p className="text-sm  mb-4">Category: {product.category}</p>
             <p className=" mb-6">Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
           </div>
 
