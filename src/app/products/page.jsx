438345b851b2bbd58867c855378759cdc25e6736
abunaim34 <farhanadnanfarabi@gmail.com>
2025-10-20 21:38:36 +0600

    product details page create

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,5 +1,6 @@
 import dbConnect from "@/lib/dbConnect";
 import Image from "next/image";
+import Link from "next/link";
 
 const Products = async () => {
     const data = dbConnect()
@@ -11,29 +12,31 @@ const Products = async () => {
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
                 {
                     products?.map(p => (<div key={p._id} className="max-w-sm bg-black rounded-xl shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300">
-                    
-                    <Image src={p.image} className="w-full h-52 object-cover" alt="product" width={400} height={300} />
 
-                    <div className="p-5 text-center">
-                        <h2 className="text-xl font-semibold mb-1">
-                            {p.name}
-                        </h2>
-                        <p className="text-sm text-gray-300 mb-2">Category: {p.category}</p>
+                        <Image src={p.image} className="w-full h-52 object-cover" alt="product" width={400} height={300} />
 
-                        <p className="text-gray-500 text-sm mb-3">
-                            {p.shortBio}
-                        </p>
+                        <div className="p-5 text-center">
+                            <h2 className="text-xl font-semibold mb-1">
+                                {p.name}
+                            </h2>
+                            <p className="text-sm text-gray-300 mb-2">Category: {p.category}</p>
 
-                        <div className="flex items-center justify-between mb-4">
-                            <span className="text-lg font-bold text-yellow-600">${p.price}</span>
-                            <span className="text-sm font-medium text-green-600">{p.status}</span>
+                            <p className="text-gray-500 text-sm mb-3">
+                                {p.shortBio}
+                            </p>
+
+                            <div className="flex items-center justify-between mb-4">
+                                <span className="text-lg font-bold text-yellow-600">${p.price}</span>
+                                <span className="text-sm font-medium text-green-600">{p.status}</span>
+                            </div>
+
+                            <button className="w-full bg-yellow-500 text-white font-medium py-2 rounded hover:bg-yellow-600 transition-colors">
+                                <Link href={p._id}>
+                                    View Details
+                                </Link>
+                            </button>
                         </div>
-
-                        <button className="w-full bg-yellow-500 text-white font-medium py-2 rounded hover:bg-yellow-600 transition-colors">
-                            View Details
-                        </button>
-                    </div>
-                </div>))
+                    </div>))
                 }
             </div>
 
