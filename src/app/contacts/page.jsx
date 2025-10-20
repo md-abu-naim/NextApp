4d6fb3a08a40d4b7c5af5f57ad104ba1413e3a01
abunaim34 <farhanadnanfarabi@gmail.com>
2025-10-20 21:55:10 +0600

    contacts page complete

--- src/app/contacts/page.jsx
+++ src/app/contacts/page.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Contacts = () => {
-    return (
-        <div className="min-h-screen  flex items-center justify-center p-4">
+  return (
+    <div className="min-h-screen  flex items-center justify-center p-4">
       <div className="bg-black rounded-xl shadow-md p-8 w-full max-w-md">
         <h1 className="text-3xl font-bold mb-6 text-center text-white">
           Contact Us
@@ -48,7 +48,7 @@ const Contacts = () => {
         </div>
       </div>
     </div>
-    );
+  );
 };
 
 export default Contacts;
